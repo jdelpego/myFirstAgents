00160561581ac806a24836734bc7891ab2532e84
jdelpego <jdelpego@gmail.com>
2025-12-26 07:58:14 -0800

    got rid of langchain tool decorator because it is not supposed to be used in langraph, just langchain

--- state_langraph.py
+++ state_langraph.py
@@ -1,21 +1,18 @@
-from langchain.tools import tool
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 
 class State(TypedDict):
     count: int
 
-@tool
-def add1(state: State) -> State:
+def add_1(state: State) -> State:
     """Adds 1 to the current count in state"""
     return {"count": state["count"] + 1}
     
 
-
 builder = StateGraph(State)
-builder.add_node(add1)
-builder.add_edge(START, add1)
-builder.add_edge(add1, END)
+builder.add_node("add_1", add_1)
+builder.add_edge(START, "add_1")
+builder.add_edge("add_1", END)
 agent = builder.compile()
 response = agent.invoke({"count": 0})
 print(response)
